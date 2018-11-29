3a24137ec0cfe7d98d542e1a891a4e59bf649634
Unknown <brad.walbeck@gmail.com>
2018-11-28 19:21:24 -0700

    changed audio file sources to github raw; added title values to HTMLAudioElements

--- src/app/services/audiotrack/audiotrack.service.ts
+++ src/app/services/audiotrack/audiotrack.service.ts
@@ -5,7 +5,7 @@ export class AudioTrackService {
 
   constructor() { }
 
-  private srcs: string[] = [ // this should fill by getting the names from the files in the folder
+  private srcs: string[] = [
     'Airport-lounge-music-for-airports'
    , 'Howling-wind'
    , 'Piano-music-free'
@@ -16,19 +16,20 @@ export class AudioTrackService {
   ];
 
   public getAll () {
-    const baseUrl = './../../assets/audio/';
+    const baseUrl = 'https://github.com/bradwalbeck/atmosphere/blob/master/docs/assets/audio/';
     const audioElements = Array<HTMLAudioElement>();
     for (const src of this.srcs) {
-      audioElements.push(this.loadAudioElement(baseUrl + src + '.mp3'));
+      audioElements.push(this.loadAudioElement(baseUrl + src + '.mp3?raw=true', src));
     }
     return audioElements;
   }
 
-  private loadAudioElement (src: string): HTMLAudioElement {
+  private loadAudioElement (src: string, title: string): HTMLAudioElement {
     const audio = new Audio();
     audio.src = src;
     audio.volume = 0.5;
     audio.loop = true;
+    audio.title = title;
     return audio;
   }
 }
