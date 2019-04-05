ac7eb9a65bb656af081eb1615af42c181e3d9a90
brad walbeck <brad.walbeck@gmail.com>
2019-04-04 20:36:17 -0600

    updated audio;

--- src/app/services/audiotrack/audiotrack.service.ts
+++ src/app/services/audiotrack/audiotrack.service.ts
@@ -5,20 +5,22 @@ export class AudioTrackService {
 
   constructor() { }
 
-  private srcs: string[] = [
-    'Airport-lounge-music-for-airports'
-   , 'Howling-wind'
-   , 'Piano-music-free'
-   , 'Soft-rain-sound'
-   , 'Six'
-   , 'MoonlightReprise'
-   , 'Spaceship Ambience'
-  ];
+  private displatedTrackTitle: string[] = [ 
+     'Station Nominal - Engineered for atmosphere'
+   , 'Aquatic Station - Engineered for atmosphere'
+   , 'Ship Engines Stand By - Engineered for atmosphere'
+   , 'Ship Engines Nominal - Engineered for atmosphere'
+   , 'Why by Ilya Malyuev - Trance Remix by Greg Baumont'
+   , 'Waiting TTTT - Minimal Ambient Bounce - Loyalty Freak Music'
+   , 'Lag - Minimal Ambient Bounce - Loyalty Freak Music'
+   , 'No Cadillac - Minimal Ambient Bounce - Loyalty Freak Music'
+   ];
 
   public getAll () {
+    //const baseUrl = '../../assets/audio/'; //local assets
     const baseUrl = 'https://github.com/bradwalbeck/atmosphere/blob/master/docs/assets/audio/';
     const audioElements = Array<HTMLAudioElement>();
-    for (const src of this.srcs) {
+    for (const src of this.displatedTrackTitle) {
       audioElements.push(this.loadAudioElement(baseUrl + src + '.mp3?raw=true', src));
     }
     return audioElements;
